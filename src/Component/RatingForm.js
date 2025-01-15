0eda67a242f1c6eb5d51838ca18b4ce63a622c8a
vb8146649 <vb8146649@gmail.com>
2025-01-16 01:37:47 +0530

    restructuring html , added fieldset

--- src/Component/RatingForm.js
+++ src/Component/RatingForm.js
@@ -1,12 +1,27 @@
 import { useState } from "react";
 
-function RatingForm({setNum}) {
-  const buttons = { 
-    1: ["peer-checked/rating1:bg-orange-500 peer-focus/rating1:border peer-checked/rating1:text-black","peer/rating1"],
-    2: ["peer-checked/rating2:bg-orange-500 peer-focus/rating2:border peer-checked/rating2:text-black","peer/rating2"], 
-    3: ["peer-checked/rating3:bg-orange-500 peer-focus/rating3:border peer-checked/rating3:text-black","peer/rating3"], 
-    4: ["peer-checked/rating4:bg-orange-500 peer-focus/rating4:border peer-checked/rating4:text-black","peer/rating4"], 
-    5: ["peer-checked/rating5:bg-orange-500 peer-focus/rating5:border peer-checked/rating5:text-black","peer/rating5"],
+function RatingForm({ setNum }) {
+  const buttons = {
+    1: [
+      "peer-checked/rating1:bg-orange-500 peer-focus/rating1:border peer-checked/rating1:text-black",
+      "peer/rating1",
+    ],
+    2: [
+      "peer-checked/rating2:bg-orange-500 peer-focus/rating2:border peer-checked/rating2:text-black",
+      "peer/rating2",
+    ],
+    3: [
+      "peer-checked/rating3:bg-orange-500 peer-focus/rating3:border peer-checked/rating3:text-black",
+      "peer/rating3",
+    ],
+    4: [
+      "peer-checked/rating4:bg-orange-500 peer-focus/rating4:border peer-checked/rating4:text-black",
+      "peer/rating4",
+    ],
+    5: [
+      "peer-checked/rating5:bg-orange-500 peer-focus/rating5:border peer-checked/rating5:text-black",
+      "peer/rating5",
+    ],
   };
   // const [rating,setRating]=useState(null);
   let rating = null;
@@ -33,43 +48,45 @@ function RatingForm({setNum}) {
           />
         </svg>
       </div>
-      <h1 className="text-white text-2xl mt-4 font-semibold ">
-        How did we do?
-      </h1>
-      <p className="mt-4">
-        Please let us know how we did with your support request. All feedback is
-        appreciated to help us improve our offering!
-      </p>
-      <div className="flex justify-between mt-4">
-        {Object.entries(buttons).map(function ([key,item]) {
-          return (
-            <>
-              <input
-                type="radio"
-                id={"rating" + key}
-                className={`${item[1]} appearance-none sr-only `}
-                name="rating"
-                defaultValue={key}
-                required=""
-                aria-required="true"
-                onChange={(e)=>(rating=e.target.value)}
-              />
-              <label
-                htmlFor={"rating" + key}
-                className={`block w-12 h-12 rounded-full text-center content-center hover:bg-white hover:text-black ${item[0]} bg-[hsl(213,19%,18%)]`}
-              >
-                {key}
-              </label>
-            </>
-          );
-        })}
-      </div>
-      <button
-        type="submit"
-        className="bg-orange-500 mt-4 text-black w-full p-3 tracking-widest hover:bg-white rounded-full font-bold "
-      >
-        SUBMIT
-      </button>
+      <fieldset className="mt-4">
+        <legend className="text-white text-2xl block font-semibold ">
+          How did we do?
+        </legend>
+        <p className="mt-4">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <div className="flex justify-between mt-4">
+          {Object.entries(buttons).map(function ([key, item]) {
+            return (
+              <>
+                <input
+                  type="radio"
+                  id={"rating" + key}
+                  className={`${item[1]} appearance-none sr-only `}
+                  name="rating"
+                  defaultValue={key}
+                  required=""
+                  aria-required="true"
+                  onChange={(e) => (rating = e.target.value)}
+                />
+                <label
+                  htmlFor={"rating" + key}
+                  className={`block w-12 h-12 rounded-full text-center content-center hover:bg-white hover:text-black ${item[0]} bg-[hsl(213,19%,18%)]`}
+                >
+                  {key}
+                </label>
+              </>
+            );
+          })}
+        </div>
+        <button
+          type="submit"
+          className="bg-orange-500 mt-4 text-black w-full p-3 tracking-widest hover:bg-white rounded-full font-bold "
+        >
+          SUBMIT
+        </button>
+      </fieldset>
     </form>
   );
 }
